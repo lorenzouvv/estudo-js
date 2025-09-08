ec134f7e4706628efcc87f9be523fc6227e63364
Lorenzo Simonassi <lorenzo.moura@uvv.br>
2025-09-08 18:01:58 -0300

    aula 11

---
+++ aula11/ex007.js
@@ -0,0 +1,6 @@
+var vel = 102;
+if (vel > 60) {
+    console.log(`cuidado, você está acima da velocidade máxima permitida. MULTADO!`)
+}
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+console.log(`Dirija sempre usando cinto de segurança!`);
